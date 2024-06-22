3dd2cc02f2bf91ddcd183b0dcc54f76b34409c4f
최정우 <chja21@naver.com>
2024-06-22 17:33:48 +0900

    Clean up working tree

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,5 +9,21 @@ export class AppController {
   @Render('index')
   root() {
     return this.appService.getHello();
+  }
+
+  @Get('/init')
+  async init(
+    @Query('user') user: string,
+    @Query('userval') userval: string,
+  ): Promise<string> {
+    return this.appService.init(user, userval);
+  }
+
+  @Get('/chargeMoney')
+  async chargeMoney(
+    @Query('userInfo') userInfo: string,
+    @Query('amount') amount: string,
+  ): Promise<string> {
+    return this.appService.init(userInfo, amount);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { send } from './util/connectFabric';
 
 @Injectable()
 export class AppService {
   getHello() {
     return {message: 'Hello World!'};
   }
+
+  async init(user:string, userval:string){
+    const args = [user, userval];
+    return await send(false, 'init', args);
+  }
+
+
+  async chargeMoney(userInfo:string, amount:string){
+    const args = [userInfo, amount];
+    return await send(false, 'cahrgeMoney', args);
+  }
+
+
 }
